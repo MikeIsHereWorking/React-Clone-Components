f1668c7572c4e6c1e90ef401a3d84938f4624a72
Mike Bova <mbova407@yahoo.com>
2019-02-21 15:47:58 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Container from './styles'
+
+class App extends React.Component {
+    state = {}
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <Container>
+                <div></div>
+                <div>LIST</div>
+        </Container>
+    }
+}
