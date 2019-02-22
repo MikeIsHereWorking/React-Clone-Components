022646896b231ebe0fa8cc6626b035355e208e87
Mike Bova <mbova407@yahoo.com>
2019-02-22 10:15:44 -0500

    Progress with Data

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export default {
+  videoURL:
+    "https://s3-us-west-1.amazonaws.com/youtube-clone-assets/Clone+Components+-+YouTube+Tags+Part+3.mp4",
+  duration: 458,
+  description: `This is the description for Elephants Dreaming. Hey even they deserve there dreams. Even though this portion of the description goes way longer we can set white-space to pre-wrap, its cool.
+
+How do the spaces translate to the HTML?
+    
+What is happening. Okay i think it is time to test this stuff:
+
+First really smart thing 00:45
+Another genius point is made 1:21
+Shows true colors here 02:02
+Confirms stupidity 2:53
+Another random time 4:30
+What if time is longer than video? 10:00
+What if time does not exist? 02:90
+
+How about some links to other stuff: 
+
+Subscribe: http://goo.gl/dYpsgH
+
+Check out our full video catalog: http://goo.gl/u8VvLi
+Visit our playlists: http://goo.gl/eFhsvM
+Like MMAF on Facebook: http://goo.gl/uhdg7Z
+Follow on Twitter: http://goo.gl/nOATUI
+Read More: http://www.mmafighting.com
+Subscribe to the podcast: http://applepodcasts.com/mmahour
+
+MMA Fighting is your home for exclusive interviews, live shows, and more for one of the world's fastest-growing sports. Get latest news and more here: http://www.mmafighting.com
+  
+Lastly, hashtags can be used for things like search parameters. Setup is similar to time but you would parse the term and then execute the search on page load
+
+Checkout #react and other #javascript videos that will blow your mind!
+
+Cool?
+  `,
+  list: [
+    "react",
+    "preact",
+    "javascript",
+    "react router",
+    "ruby",
+    "array",
+    "javascript prototypes",
+    "object",
+    "function",
+    "react native",
+    "error",
+    "angular",
+    "vue",
+    "react and redux",
+    "variable"
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import Container from './styles'
+import data from './data';
 
 class App extends React.Component {
     state = {}
@@ -12,8 +13,15 @@ class App extends React.Component {
 
     render() {
         return <Container>
-                <div></div>
+                <div>
+                    <video src={data.videoURL} controls />
+                    <div>{data.description}</div>
+                    <div>show more</div>
+                </div>
                 <div>LIST</div>
         </Container>
     }
 }
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
